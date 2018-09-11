214342664c6a7cadd2735032269b0a510e480100
Kevin Sammut <ksammut89@gmail.com>
2018-09-12 00:36:22 +0200

    Chapter 3 files

---
+++ Lsn_03_01/global.js
@@ -0,0 +1,10 @@
+//GLOBAL OBJECT
+
+var path = require("path");
+
+// var hello = "Hello World!";
+// console.log(hello);
+// var justNode = hello.slice(6);
+console.log(`Whats Up ${path.basename(__filename)}`);
+// console.log(__dirname);
+// console.log(__filename);
--- global.js
+++
@@ -1,3 +0,0 @@
-/* jslint vars: true, plusplus: true, devel: true, debug:true, nomen: true, indent: 4, maxerr: 50, node: true */
-var hello = "Hello World";
-console.log(hello);
