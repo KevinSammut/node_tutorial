8cabe980e1567ec7bc3c8907ee49e082de4b0b07
Kevin Sammut <kevin@untangledmedia.com>
2018-09-12 09:26:38 +0200

    Old Updates

--- global.js
+++ global.js
@@ -1,3 +1,10 @@
 /* jslint vars: true, plusplus: true, devel: true, debug:true, nomen: true, indent: 4, maxerr: 50, node: true */
 var hello = "Hello World";
-console.log(hello);
+
+var justNode = hello.slice(7);
+
+console.log(`Some other text ${justNode}`);
+
+console.log(__dirname);
+
+console.log(__filename);
